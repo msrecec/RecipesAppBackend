dc4801b6a47f99f6cb046944808a1b23b43693bf
msrecec <67606889+msrecec@users.noreply.github.com>
2021-10-07 13:59:33 +0200

    added pagination

--- src/main/java/com/backend/recipes/rest/IngredientController.java
+++ src/main/java/com/backend/recipes/rest/IngredientController.java
@@ -3,6 +3,7 @@ package com.backend.recipes.rest;
 import com.backend.recipes.command.ingredient.IngredientSaveCommand;
 import com.backend.recipes.command.ingredient.IngredientUpdateCommand;
 import com.backend.recipes.dto.ingredient.IngredientDTO;
+import com.backend.recipes.dto.ingredient.IngredientDTOPaginated;
 import com.backend.recipes.service.ingredient.IngredientService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/v1/ingredient")
@@ -24,6 +26,26 @@ public class IngredientController {
     @GetMapping
     public List<IngredientDTO> getAllIngredients() {
         return ingredientService.findAll();
+    }
+
+    @GetMapping("/page/{page}")
+    public ResponseEntity<IngredientDTOPaginated> getIngredientPaginated(@PathVariable final Integer page) {
+        if(page < 0) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        return ingredientService.findPaginated(page)
+                .map(
+                        ingredientDTOPaginated -> ResponseEntity
+                                .status(HttpStatus.OK)
+                                .body(ingredientDTOPaginated)
+                )
+                .orElseGet(
+                        () -> ResponseEntity
+                                .status(HttpStatus.NOT_FOUND)
+                                .build()
+                );
+
     }
 
     @GetMapping("/id/{id}")
